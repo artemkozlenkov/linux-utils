7345c2d4ce2b749c091939f2ca5df6838ffc1f9f
artem kozlenkov <artem.kozlenkov@gmail.com>
2021-04-13 17:38:25 +0200

    description

--- kernel-update.sh
+++ kernel-update.sh
@@ -1,3 +1,10 @@
+#!/bin/bash -e 
+
+# to use the script add executive permissions and apply with flag '-i' for install, '-r' for remove
+# e.g ./kernel-update.sh -i
+
+sudo apt update
+
 if [ ! -z $1 ] && [ $1 == "-i" ]; then 
 sudo apt install mainline
 sudo add-apt-repository --remove ppa:cappelikan/ppa
