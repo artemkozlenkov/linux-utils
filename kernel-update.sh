72bf707840878044fce2faf7aa4f7a0bac83fb34
artem kozlenkov <artem.kozlenkov@gmail.com>
2021-04-13 17:47:16 +0200

    removed flags

--- kernel-update.sh
+++ kernel-update.sh
@@ -5,10 +5,8 @@
 
 sudo apt update
 
-if [ ! -z $1 ] && [ $1 == "-i" ]; then 
 sudo apt install mainline
 sudo add-apt-repository --remove ppa:cappelikan/ppa
-echo "install"
-elif [ ! -z $1 ] && [ $1 == "-r" ]; then 
-sudo apt remove mainline
-fi;
+
+#uncomment for deletion
+#sudo apt remove mainline
