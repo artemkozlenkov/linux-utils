079dae84df2e32fea178b71e72b8e090d6d325dc
Artem Kozlenkov <37898216+artemkozlenkov@users.noreply.github.com>
2021-04-15 18:07:21 +0200

    Update util.sh

--- util.sh
+++ util.sh
@@ -3,3 +3,6 @@
 # change date of the commit 
 GIT_COMMITTER_DATE="$(date)" git commit --amend --no-edit --date "$(date)"
 git rebase --continue
+
+#speed check, python a prerequisite
+curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python -
