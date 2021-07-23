d94b3d8ab9413aae9945f8ca97d1dde167fee9e1
Artem Kozlenkov <37898216+artemkozlenkov@users.noreply.github.com>
2021-07-23 14:10:38 +0200

    Update util.sh

--- util.sh
+++ util.sh
@@ -13,3 +13,5 @@ echo 'some asci' | tee -a >> ${test_file}.txt
 #copy to buffer without empty spaces and tabs
 cat ${test_file}.txt | sed 's/ *$//' | tr -d " \t\n\r" |  xclip -sel clip
 
+# reduce pdf file size (opt: /ebook /screen /printer)
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile=output.pdf input.pdf
